eab895e6bfe1f041ee0d5d1ef14f66934eae86db
Matthew Gordon Bauman <mgbauman@ucalgary.ca>
2022-04-23 16:52:14 -0600

    Resized the navigation indicator on window resize. Renamed some functions and added light documentation

--- cityNav.js
+++ cityNav.js
@@ -1,18 +1,27 @@
-async function getCities() {
-  let request = await fetch("./Files/navigation.json");
+/**
+ * Retrieves JSON data and then adds it to the navigation.
+ */
+async function populate() {
+  const request = await fetch("./Files/navigation.json");
   const cities = await request.json();
   populateNavigation(cities);
 }
 
-function populateNavigation(obj) {
+/**
+ * Populates the navigation with the cities retrieved from the JSON file.
+ *
+ * @param jsonResponse The retrieved JSON data.
+ */
+function populateNavigation(jsonResponse) {
   const unorderedListElement = document.querySelector('#cities');
-  const cities = obj['cities'];
+  const cities = jsonResponse['cities'];
 
   for (const city of cities) {
     const linkTag= document.createElement('a');
     linkTag.className = 'nav-item';
     linkTag.href = '#';
     linkTag.textContent = city.label;
+
     linkTag.addEventListener("click", function(event) {
       let current = document.querySelector(".nav-item.active");
       if (current) {
@@ -26,10 +35,20 @@ function populateNavigation(obj) {
   }
 }
 
+/**
+ * Resizes the navigation indicator to match the selected item.
+ *
+ * @param target the selected item in the navigation.
+ */
 function resizeNavigationIndicator(target) {
   const navigationIndicator = document.querySelector('#navigation-indicator');
   navigationIndicator.style.width = target.offsetWidth + 'px';
   navigationIndicator.style.left = target.offsetLeft + 'px';
 }
 
-getCities();
+window.addEventListener("resize", function(event) {
+  const activeItem = document.querySelector('.nav-item.active');
+  resizeNavigationIndicator(activeItem);
+});
+
+populate();
