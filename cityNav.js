df370be4e07d12d6726b805adc773b15fed8ac3f
Matthew Gordon Bauman <mgbauman@ucalgary.ca>
2022-04-23 16:13:20 -0600

    Added initial version of sliding nav indicator. Still needs window resizing

--- cityNav.js
+++ cityNav.js
@@ -13,16 +13,23 @@ function populateNavigation(obj) {
     linkTag.className = 'nav-item';
     linkTag.href = '#';
     linkTag.textContent = city.label;
-    linkTag.addEventListener("click", function() {
+    linkTag.addEventListener("click", function(event) {
       let current = document.querySelector(".nav-item.active");
       if (current) {
         current.className = current.className.replace(" active", "");
       }
       this.className += " active";
+      resizeNavigationIndicator(event.target);
     });
 
     unorderedListElement.appendChild(linkTag);
   }
+}
+
+function resizeNavigationIndicator(target) {
+  const navigationIndicator = document.querySelector('#navigation-indicator');
+  navigationIndicator.style.width = target.offsetWidth + 'px';
+  navigationIndicator.style.left = target.offsetLeft + 'px';
 }
 
 getCities();
