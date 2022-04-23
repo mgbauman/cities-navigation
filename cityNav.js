c38bc01e68b84bc638ac28c67e6d5fedcad820df
Matthew Gordon Bauman <mgbauman@ucalgary.ca>
2022-04-23 12:27:05 -0600

    Added populateNavigation function to populate the citiesList with the cities retrieved from the JSON data

--- cityNav.js
+++ cityNav.js
@@ -2,7 +2,22 @@
 async function getCities() {
   let request = await fetch("./Files/navigation.json");
   const cities = await request.json();
-  console.log(cities);
+  populateNavigation(cities);
+}
+
+function populateNavigation(obj) {
+  const unorderedListElement = document.querySelector('#cities');
+  const cities = obj['cities'];
+
+  for (const city of cities) {
+    const listItem = document.createElement('li');
+    const linkTag= document.createElement('a');
+
+    linkTag.textContent = city.label;
+    listItem.appendChild(linkTag);
+
+    unorderedListElement.appendChild(listItem);
+  }
 }
 
 getCities();
